2f2a6a68e93d2a3526a40dc2e6f4859cf37f1203
Diego Alejandro Asencio Cuellar <diego.asencio@rappi.com>
2020-04-26 17:04:31 -0500

    initial scripts

--- docker-engine.sh
+++ docker-engine.sh
@@ -4,4 +4,4 @@ sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent softwa
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo apt-key fingerprint 0EBFCD88
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
